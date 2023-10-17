418dc3cbdc12c33d7e198aab9c02d2a5224b54da
harshmanu22900 <harshmanu22900@gmail.com>
2023-10-17 06:49:54 +0000

    hi, commit all

---
+++ linkedlist.java
@@ -0,0 +1,16 @@
+package day3;
+import java.util.*;
+public class linkedlist {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer>ll=new LinkedList<Integer>();
+		ll.add(87);
+		ll.add(98);
+		ll.add(80);
+		ll.add(50);
+		System.out.println(ll);
+	}
+
+}
